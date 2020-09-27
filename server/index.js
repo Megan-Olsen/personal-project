33705ef88bbdc1297d0cfcd8c6f500f512147808
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-26 22:36:21 -0600

    .envconnections

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive')
+const ctrl = require('./controllers/controller');
+
+
+const app = express()
+
+const {CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env
+
+app.use(express.json())
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie:{maxAge: 1000 * 60 * 60 * 24 * 365}
+    })
+)
+
+
+//endpoints.... 
+
+
+
+
+
+
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(dbInstance => {
+    app.set('db', dbInstance)
+    console.log('Database is working right now')
+    app.listen(SERVER_PORT, () => console.log(`Running dungeon in door ${SERVER_PORT}`))
+}).catch(error => console.log('massive not functioning right'))
