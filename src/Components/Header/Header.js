255d6b5934078f6931e846ce83cedb7959f24a7d
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-28 13:42:51 -0600

    header?

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        <nav className="nav-header">
+            <h1>The Gloomhaven Codex</h1>
+        </nav>
+    )
+}
+export default Header;
