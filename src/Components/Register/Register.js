38fb38d789de95a30c0e4ad425fd13a99443228a
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-28 12:15:38 -0600

    registerpagedone

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,19 +1,67 @@
 import React, {Component} from 'react';
+import axios from 'axios'
+import { loginUser } from '../../ducks/authReducer'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 
 class Register extends Component {
-
-
+    constructor(){
+        super()
+        this.state = {
+            username: '',
+            email: '',
+            password: ''
+        }
+    }
+    
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    handleRegister = () => {
+        const { username, email, password } = this.state
+        axios.post('./api/auth/register', { username, email, password }).then((res) => {
+            this.props.loginUser(res.data)
+            const { userid } = res.data
+            this.props.history.push(`/account/${userid}`)
+        }).catch((err) => {
+            alert('cannot register')
+        })
+    }
     render(){
         return(
-            <div>
-                <p>Register</p>
-
-
-
-
-
+            <div className="register">
+                <div className="input-container">
+                    <div className="inputs">
+                        <div className="inputsboxes">
+                            <label>
+                                <p>Username:</p>
+                                <input maxLength="50" name="username" onChange={(e) => { this.handleInput(e)}}/>
+                            </label>
+                            <label>
+                                <p>Email:</p>
+                                <input name="email" type="email" onChange={(e) => { this.handleInput(e)}}/>
+                            </label>
+                            <label>
+                                <p>Password:</p>
+                                <input type="password" maxLength="20" name="password" onChange={(e) => { this.handleInput(e)}}/>
+                            </label>
+                        </div>
+                        <button onClick={() => {
+                            this.handleRegister()
+                        }} className="register-button">Register</button>
+                    </div>
+                    <div className="linktologin">
+                        <span>Already have an account? Login </span>
+                        <Link className="redirect" to="/">
+                            here
+                        </Link>
+                    </div>
+                </div>
             </div>
     )}
 }
-export default Register;
+export default connect(null, { loginUser })(Register);
