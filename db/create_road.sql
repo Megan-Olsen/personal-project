c49307332c3d24fbb84471efab81747aa259c24d
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-04 20:34:01 -0600

    create road

---
+++ db/create_road.sql
@@ -0,0 +1,25 @@
+INSERT INTO roaddeck (road1, road2,	road3,	road4,	road5,	road6,	road7,	road8,	road9,	road10,	road11,	road12,	road13,	road14,	road15,	road16,	road17,	road18,	road19,	road20,	road21,	road22,	road23,	road24,	road25,	road26,	road27,	road28,	road29,	road30,	road31,	road32,	road33,	road34,	road35,	road36,	road37,	road38,	road39,	road40,	road41,	road42,	road43,	road44,	road45,	road46,	road47,	road48,	road49,	road50,	road51,	road52,	road53,	road54,	road55,	road56,	road57,	road58,	road59,	road60,	road61,	road62,	road63,	road64,	road65,	road66,	road67,	road68,	road69)
+VALUES ('Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Active', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive', 'Inactive')
+returning roaddeckid;
+
+
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card30 TO road30;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card31 TO road31;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card32 TO road32;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card33 TO road33;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card34 TO road34;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card35 TO road35;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card36 TO road36;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card37 TO road37;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card38 TO road38;
+-- ALTER TABLE roaddeck
+-- RENAME COLUMN card39 TO road39;
