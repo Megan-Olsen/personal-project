376d84f0299d5f2672ff93c2c7f0e12367882a69
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-09 10:27:44 -0600

    contact about

--- src/Components/Contact/AboutUs.js
+++ src/Components/Contact/AboutUs.js
@@ -14,7 +14,7 @@ const AboutUs = () => {
 <br/><br/>
 This is a game with a persistent and changing world that is ideally played over many game sessions. After a scenario, players will make decisions on what to do, which will determine how the story continues, kind of like a “Choose Your Own Adventure” book. Playing through a scenario is a cooperative affair where players will fight against automated monsters using an innovative card system to determine the order of play and what a player does on their turn.
 <br/><br/><br/>
-This page was made to help track the scenarios and party notes. To spend more time playing and less time deciphering scratch paper notes.</p>
+This website was made to help track the scenarios and party notes. To spend more time playing and less time deciphering scratch paper notes.</p>
 
         </div>
     )
