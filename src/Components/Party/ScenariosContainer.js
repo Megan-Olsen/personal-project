e6224b056cc2d43e7ca531a1ad46efebc284a812
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-09 05:09:04 -0600

    achievements box

--- src/Components/Party/ScenariosContainer.js
+++ src/Components/Party/ScenariosContainer.js
@@ -4,47 +4,48 @@ import {getScenarios} from '../../ducks/partyReducer'
 
 
 class ScenariosContainer extends Component {
-//   constructor(){
-//     super()
-//     this.state ={
-//       completedScenarios: [],
-//       lockedScenarios: [],
-//       unlockedScenarios: []
-//     }
-//   }
-//    componentDidMount(){
-//      this.getCoScenarios()
-//      this.getLoScenarios()
-//      this.getUlScenarios()
-//   }
+  constructor(){
+    super()
+    this.state ={
+      completedScenarios: [],
+      lockedScenarios: [],
+      unlockedScenarios: []
+    }
+  }
+   componentDidMount(){
+     this.props.getScenarios().then(()=>{
+     this.getCoScenarios()
+     this.getLoScenarios()
+     this.getUlScenarios()})
+  }
 
-//   getCoScenarios(){
-//     let scenarios = this.props.partr.scenarios
-//     let completedScenarios = []
-//     for( let prop in scenarios){
-//     if(scenarios[prop] === "Completed"){
-//         completedScenarios.push(prop)
-//     }}
-//     return completedScenarios
-// }
-// getLoScenarios(){
-//   let scenarios = this.props.partr.scenarios
-//   let lockedScenarios = []
-//   for( let prop in scenarios){
-//   if(scenarios[prop] === "Locked"){
-//       lockedScenarios.push(prop)
-//   }}
-//   return lockedScenarios
-// }
-//   getUlScenarios(){
-//     let scenarios = this.props.partr.scenarios
-//     let unlockedScenarios = []
-//     for( let prop in scenarios){
-//     if(scenarios[prop] === "Unlocked"){
-//         unlockedScenarios.push(prop)
-//     }}
-//     return unlockedScenarios
-// }
+  getCoScenarios(){
+    let scenarios = this.props.partr.scenarios
+    let completedScenarios = []
+    for( let prop in scenarios){
+    if(scenarios[prop] === "Completed"){
+        completedScenarios.push(prop)
+    }}
+    return completedScenarios
+}
+getLoScenarios(){
+  let scenarios = this.props.partr.scenarios
+  let lockedScenarios = []
+  for( let prop in scenarios){
+  if(scenarios[prop] === "Locked"){
+      lockedScenarios.push(prop)
+  }}
+  return lockedScenarios
+}
+  getUlScenarios(){
+    let scenarios = this.props.partr.scenarios
+    let unlockedScenarios = []
+    for( let prop in scenarios){
+    if(scenarios[prop] === "Unlocked"){
+        unlockedScenarios.push(prop)
+    }}
+    return unlockedScenarios
+}
 
 
   render() {
@@ -52,9 +53,6 @@ class ScenariosContainer extends Component {
       return(
           <div className="scenarioscontainer">
             <p>Scenarios</p>
-      <h2>Locked Scenarios</h2><p></p>
-      <h2>Completed Scenarios</h2><p></p>
-      <h2>Unlocked Scenarios</h2><p></p>
           </div>
 
       )
