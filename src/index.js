fa81fa28b5da52b099004bcfaeb132464849c91b
Megan Olsen <megan.a.olsen63@gmail.com>
2020-10-17 11:19:58 -0600

    router

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { Provider } from 'react-redux';
 import store from './ducks/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
