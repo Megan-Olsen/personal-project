30d4aa5ae8f507f64a45fbdc8465be050dc62b42
Megan Olsen <megan.a.olsen63@gmail.com>
2020-11-04 13:59:09 -0700

    make spaces tinier

--- src/Components/Account/UsPCon.js
+++ src/Components/Account/UsPCon.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import UserParty from './UserParty'
-
-
 class UsPCon extends Component {
 constructor(props){
     super(props)
@@ -14,8 +12,6 @@ constructor(props){
 componentDidMount(){
     this.getParties()
 }
-
-
 getParties(props) {
     const userid = this.props.auth.user.userid
     axios.post('/api/userchar', {userid}).then((res) => {
@@ -25,19 +21,15 @@ getParties(props) {
     })
 }
 render(){
-
     const mappedCharsu = this.state.characters.map((character, index) => {
         return(
             <UserParty character={character} key={character.id} />
             )
         })
-                    return(
-
-
-<div>
-{mappedCharsu}
-</div>
+        return(
+            <div>
+                {mappedCharsu}
+            </div>
 )}}
 const mapStateToProps = (state) => state
-
 export default connect(mapStateToProps)(UsPCon);
